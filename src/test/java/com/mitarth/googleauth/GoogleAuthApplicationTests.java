2f5388d749714f6b96341ac2a81a9dda7d1bbf8a
mitarth pathak <97668137+mitarthpathak@users.noreply.github.com>
2026-07-23 22:55:05 +0530

    the first commit

---
+++ src/test/java/com/mitarth/googleauth/GoogleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitarth.googleauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
